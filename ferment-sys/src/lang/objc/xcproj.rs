688595e871e403d9c6796ac040af57fe3652c7ba
pankcuf <rastacake@gmail.com>
2024-10-15 09:19:14 +0700

    chore: objc config shorthand

--- ferment-sys/src/lang/objc/xcproj.rs
+++ ferment-sys/src/lang/objc/xcproj.rs
@@ -5,6 +5,11 @@ pub struct Config {
     pub framework_name: String,
     pub header_name: String,
 }
+impl Config {
+    pub fn new(class_prefix: &str, framework_name: &str, header_name: &str) -> Self {
+        Self { class_prefix: String::from(class_prefix), framework_name: String::from(framework_name), header_name: String::from(header_name) }
+    }
+}
 impl Display for Config {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("[objc::Config]\n\tframework_name: {},\n\theader_name: {},\n\tclass_prefix: {}", self.framework_name, self.header_name, self.class_prefix))
